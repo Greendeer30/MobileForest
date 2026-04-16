a8d0c1301fe75f5f9b6561433762e96bd7d39ad4
Greendeer30 <markdtgreen@gmail.com>
2026-04-16 14:59:37 -0400

    Mobile

---
+++ script/index.js
@@ -0,0 +1,19 @@
+//DROP DOWNS
+
+/*var idSelect = document.getElementById("hunterID");
+var initialSelect = document.getElementById("hunterInitial");
+
+for (var i = 0; i<=40; i++){
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = i;
+    idSelect.appendChild(opt);
+}
+
+var initials = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (var i = 0; i<initials.length; i++){
+    var opt = document.createElement('option');
+    opt.value = initials.charAt(i);
+    opt.innerHTML = initials.charAt(i);
+    initialSelect.appendChild(opt);
+}*/
