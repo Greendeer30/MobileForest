746e9b214d44097214c21daa8db6428e8cb525d4
Greendeer30 <markdtgreen@gmail.com>
2026-04-23 15:55:29 -0400

    Firestore for Checkout Pages

--- script/checkout.js
+++ script/checkout.js
@@ -4,9 +4,35 @@ const BUTTONBUCKS = document.getElementById("buttonBucks");
 const TOTALCAUGHT = document.getElementById("totalCaught");
 const CHECKOUT = document.getElementById("checkout");
 
+var total = 0;
+
 function updateTotal(){
-    var total = Number(BUCKS.value) + Number(DOE.value) + Number(BUTTONBUCKS.value);
+    total = Number(BUCKS.value) + Number(DOE.value) + Number(BUTTONBUCKS.value);
     TOTALCAUGHT.textContent = "Total Caught: " + total;
+}
+
+async function updateData(){
+
+    console.log(String(sessionStorage.getItem("id")));
+
+    const doc = await db
+        .collection("hunters")
+        .doc(String(sessionStorage.getItem("id")));
+   
+    const startTime = await doc.startTime;
+    const endTime = (new Date()).toISOString();
+
+    console.log(startTime);
+
+    await doc.collection("sessions").doc("2").set({
+        bucks: Number(BUCKS.value),
+        doe: Number(DOE.value),
+        buttonBucks: Number(BUTTONBUCKS.value),
+        caught: total
+    });
+
+    window.location.href = "donecheckout.html";
+
 }
 
 DOE.addEventListener("input", () => {
@@ -21,11 +47,9 @@ BUTTONBUCKS.addEventListener("input", () => {
     updateTotal();
 });
 
+
 CHECKOUT.addEventListener("click", () => {
 
+    updateData();
 
-
-    if(ID.value >= 1 && ID.value <= 99){
-        checkLoginInfo(ID.value, NAME.value);
-    }
 });
